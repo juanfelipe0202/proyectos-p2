f3cd00a95aa3808a6e54d5642535613c597fde89
juanfelipe0202 <JuanDiez0202@gmail.com>
2024-12-06 12:28:40 -0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,19 @@
 import discord
+import random
 from discord.ext import commands
 
+chistes = [
+    "¿Por qué los pájaros no usan Facebook? Porque ya tienen Twitter.",
+    "¿Qué hace una abeja en el gimnasio? Zum-ba.",
+    "¿Por qué los esqueletos no pelean entre ellos? Porque no tienen agallas.",
+    "¿Cómo se despiden los químicos? Ácido un placer.",
+    "¿Qué le dice una iguana a su hermana gemela? Iguanita."
+]
+    
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='.b ', intents=intents)
+bot = commands.Bot(command_prefix='/b ', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -21,3 +30,12 @@ async def heh(ctx, count_heh = 5):
 @bot.command()
 async def jaja(ctx, name, count_heh = 5):
     await ctx.send(name + "se cayó al piso" + "jajaja" * count_heh)
+    
+@bot.command()
+async def registrar_evento(ctx, nombre_evento: str, fecha: str, hora: str, ubicacion: str):
+    await ctx.send(f'¡Evento registrado!\nNombre del evento: {nombre_evento}\nFecha: {fecha}\nHora: {hora}\nUbicación: {ubicacion}')
+    
+@bot.command()
+async def chiste(ctx):
+    chiste_aleatorio = random.choice(chistes)
+    await ctx.send(chiste_aleatorio)
